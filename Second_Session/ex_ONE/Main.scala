d23be5ba6cfa6959e23d361c03158ff04b02b132
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2023-07-02 19:43:45 +0100

    Add ex_One of Day 2

---
+++ Second_Session/ex_ONE/Main.scala
@@ -0,0 +1,28 @@
+object Main {
+  def word_occurrence(words: Array[String]): String = {
+    var mostOccurrenceWord: String = words.head
+    var mostOccurrenceWordCounter: Integer = 0
+    for (word <- words) {
+      var counter: Integer = 0
+      for (_elem <- words) {
+        if (_elem.equals(word)) {
+          counter += 1
+        }
+      }
+      // Check for occurrences
+      if (counter > mostOccurrenceWordCounter) {
+        mostOccurrenceWord = word
+        mostOccurrenceWordCounter = counter
+      }
+    }
+
+    mostOccurrenceWord
+  }
+
+  def main(args: Array[String]): Unit = {
+    var keywords = Array("apple", "banana", "cherry", "banana", "apple", "date", "apple")
+    var mostOccurredWord: String = word_occurrence(keywords)
+
+    println(mostOccurredWord)
+  }
+}
