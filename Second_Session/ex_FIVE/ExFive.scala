efb20d33c269a75d669ec3a31f5f9c3d2ec49307
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2023-07-02 22:24:43 +0100

    Minor changes exercise five

--- Second_Session/ex_FIVE/ExFive.scala
+++ Second_Session/ex_FIVE/ExFive.scala
@@ -1,11 +1,11 @@
 object ExFive {
 
-  def sortByLengthAndLexico(word1: String, word2: String): Boolean = {
+  private def sortByLengthAndLexico(word1: String, word2: String): Boolean = {
     word1.length > word2.length && word1.compareTo(word2) > 0
   }
 
   def main(args: Array[String]): Unit = {
     val words = List("apple", "banana", "date", "cherry", "fig")
-    println((words.sortWith(sortByLengthAndLexico).mkString("Array(", ", ", ")")))
+    println(words.sortWith(sortByLengthAndLexico).mkString("Array(", ", ", ")"))
   }
 }
